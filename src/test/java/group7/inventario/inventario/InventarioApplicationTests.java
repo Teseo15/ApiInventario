86ec71fdd852f5f22c17b7838240191e7089f51b
Brayan  Blas <brayan.blas.egoavil30@gmail.com>
2021-04-16 14:22:25 -0500

    Initial commit

---
+++ src/test/java/group7/inventario/inventario/InventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package group7.inventario.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
